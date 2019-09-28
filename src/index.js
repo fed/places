9fce1b39c4914bbe55f39d1e0943c81cbadc1c04
Federico Knüssel <fknussel@gmail.com>
2019-09-28 11:16:57 +0200

    Unregister service worker

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './state/store';
-import registerServiceWorker from './utils/worker';
+import { unregister } from './utils/worker';
 import './index.css';
 
 store.subscribe(props => {
   ReactDOM.render(<App {...props} />, document.getElementById('root'));
 });
 
-registerServiceWorker();
+unregister();
